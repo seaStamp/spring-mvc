41d98d9947c91afdc04899a8d1cb7daa628f88a1
seastamp <seastamp10@gmail.com>
2023-11-06 00:43:56 +0900

    feat: HTTP 데이터를 객체로 처리

---
+++ src/main/java/com/sparta/springmvc/requset/Star.java
@@ -0,0 +1,11 @@
+package com.sparta.springmvc.requset;
+
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
